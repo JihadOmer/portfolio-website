2a8cf2a4dc8c27baba285114aa73480946f7c2d2
Jihad Omer <jihadomer940@gmail.com>
2023-08-03 10:22:32 +0100

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,17 +19,17 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-         I enrolled in a coding bootcamp and learned{" "}
+        I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect and building something great. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
+        problem-solving aspect and building something great. I{" "}
+        <span className="underline">love</span> the feeling of finally figuring
+        out a solution to a problem. My core stack is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and PostgresSQL
+          React, Next.js, Node.js, Express.js and PostgresSQL
         </span>
-        . I am also familiar with TypeScript. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        . I am also familiar with TypeScript. I am always looking to learn new
+        technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
